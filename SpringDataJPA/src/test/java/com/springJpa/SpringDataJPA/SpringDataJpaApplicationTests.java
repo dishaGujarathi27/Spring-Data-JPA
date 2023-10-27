f7f0ad9d8c1186d25edacee018526fd0d5033ff1
dishaGujarathi27 <disha.gujrathi@neutrinotechlabs.com>
2023-10-27 18:50:52 +0530

    Using Data JPA

---
+++ SpringDataJPA/src/test/java/com/springJpa/SpringDataJPA/SpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springJpa.SpringDataJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
